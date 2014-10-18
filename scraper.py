31a5e53fc5b9bd0271d1cb8a5108f35b0f9ae5ac
Colin Broderick <rusty@therustyproject.com>
2014-10-18 20:33:00 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,13 +15,16 @@ for i in range(len(aps_json)):
     for entry in dom.cssselect('.details-list'):
             post = {
                 'reg_ref': str(aps_json[i]['reg_ref']),
-                'date_recieved': entry.cssselect('dd')[2].text_content(),
-                'last_action': entry.cssselect('dd')[3].text_content(),
-                'application_type': entry.cssselect('dd')[4].text_content(),
-                'submission_type': entry.cssselect('dd')[5].text_content(),
-                'applicant': entry.cssselect('dd')[7].text_content(),
-                'location': entry.cssselect('dd')[8].text_content(),
-                'proposed_dev': entry.cssselect('dd')[9].text_content(),
-                'decision_due': entry.cssselect('dd')[10].text_content()
+                'date_recieved': entry[0].cssselect('dd')[2].text_content(),
+                'last_action': entry[0].cssselect('dd')[3].text_content(),
+                'application_type': entry[0].cssselect('dd')[4].text_content(),
+                'submission_type': entry[0].cssselect('dd')[5].text_content(),
+                'applicant': entry[0].cssselect('dd')[7].text_content(),
+                'location': entry[0].cssselect('dd')[8].text_content(),
+                'proposed_dev': entry[0].cssselect('dd')[9].text_content(),
+                'decision_due': entry[0].cssselect('dd')[10].text_content(),
+                'decision_date': entry[1].cssselect('dd')[0].text_content(),
+                'decision': entry[1].cssselect('dd')[1].text_content(),
+                'final_grant_date': entry[1].cssselect('dd')[2].text_content()
             }
             scraperwiki.sql.save(unique_keys, post)
